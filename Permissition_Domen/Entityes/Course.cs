849ec37945ba81fbe4adc02d3e999308252043b1
Husanboy <samadovxusan9013@gmail.com>
2024-03-05 21:00:50 +0500

    add course model

---
+++ Permissition_Domen/Entityes/Course.cs
@@ -0,0 +1,21 @@
+﻿using Permission_Domen.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permission_Domen.Entityes
+{
+    public class Course
+    {
+        public ProgrammerLanguages ProgrammerLanguages { get; set; }
+        public Teacher? Teacherid { get; set; }
+        public Student? student { get; set; }
+        public DateTime StartCourse { get; set; }
+        public DateTime EndCourse { get; set; }
+        public DateTime ClassStartTime { get; set; }
+
+
+    }
+}
