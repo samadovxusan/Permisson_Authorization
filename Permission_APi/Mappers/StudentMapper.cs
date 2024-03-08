0b747faf43117d6be9ab757a7bac7b70130e9eff
Husanboy <samadovxusan9013@gmail.com>
2024-03-08 15:53:23 +0500

    add

--- Permission_APi/Mappers/StudentMapper.cs
+++ Permission_APi/Mappers/StudentMapper.cs
@@ -1,6 +1,14 @@
-﻿namespace Permission_APi.Mappers
+﻿using AutoMapper;
+using Permission_Application.Dto_s;
+using Permission_Domen.Entityes;
+
+namespace Permission_APi.Mappers
 {
-    public class StudentMapper
+    public class StudentMapper:Profile
     {
+        public StudentMapper()
+        {
+            CreateMap<Student, StudentDTO>().ReverseMap();
+        }
     }
 }
