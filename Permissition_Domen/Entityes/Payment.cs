f1d9db851b5f7ffb8418e43e1dc63c91942c893c
Husanboy <samadovxusan9013@gmail.com>
2024-03-14 18:14:04 +0500

    add

---
+++ Permissition_Domen/Entityes/Payment.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Permission_Domen.Entityes;
+
+public class Payment
+{
+    public int Id {  get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Type { get; set; }
+    public string? Address { get; set; }
+}
