088449853ddae61125baa7278711dfddaf81f217
Husanboy <samadovxusan9013@gmail.com>
2024-03-14 18:16:56 +0500

    add repositories

---
+++ Permission_Application/Abstractions/Repositories/IPaymantRepositories.cs
@@ -0,0 +1,14 @@
+﻿using Permission_Application.Abstractions.Generic;
+using Permission_Domen.Entityes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permission_Application.Abstractions.Repositories
+{
+    public interface IPaymantRepositories:IGenericRepositories<Payment>
+    {
+    }
+}
