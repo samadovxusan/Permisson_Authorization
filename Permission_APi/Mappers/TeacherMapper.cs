ceb4498db9765b7707d084699333b190731c86ae
Husanboy <samadovxusan9013@gmail.com>
2024-03-16 19:43:36 +0500

    commit

---
+++ Permission_APi/Mappers/TeacherMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Permission_Application.Dto_s;
+
+namespace Permission_APi.Mappers
+{
+    public class TeacherMapper:Profile
+    {
+        public TeacherMapper()
+        {
+            CreateMap<TeacherMapper, TeacherDto>().ReverseMap();
+        }
+    }
+}
